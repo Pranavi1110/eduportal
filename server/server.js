3ecfcf48ecb9a60ed6cf8c8bb67d83cb50f7819a
vyshnavi arra <vyshnavi9arra@gmail.com>
2025-10-03 15:37:44 +0530

    email smtp connection setup

--- server/server.js
+++ server/server.js
@@ -83,7 +83,7 @@ if (process.env.SMTP_HOST && process.env.SMTP_USER && process.env.SMTP_PASS) {
   mailTransporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT ? parseInt(process.env.SMTP_PORT) : 587,
-    secure: process.env.SMTP_SECURE === "true",
+    secure: process.env.SMTP_SECURE === "false",
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
